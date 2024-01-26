36857bf4c8b513cdb694f759781c9a2f1ef35c90
byunn90 <kayhanturk90@Live.com>
2024-01-26 15:44:50 +1100

    friends

---
+++ split-the-bill/src/App.js
@@ -0,0 +1,35 @@
+import "./index.css";
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+export default function () {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
+}
+
+function FriendsList() {
+  return <ul>List</ul>;
+}
