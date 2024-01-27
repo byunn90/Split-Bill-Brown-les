656ae0f4c41f8b67b8db439143aba5e906e6b072
byunn90 <kayhanturk90@Live.com>
2024-01-27 17:06:28 +1100

    balance owes, button

--- split-the-bill/src/App.js
+++ split-the-bill/src/App.js
@@ -33,8 +33,22 @@ export default function App() {
 function Friend({ friend }) {
   return (
     <div>
-      <img src={friend.image} />
-      <h3>{friend.name}</h3>
+      <li>
+        <img src={friend.image} />
+        <h3>{friend.name}</h3>
+        {friend.balance < 0 && (
+          <p className="red">
+            You owe {friend.name} {Math.abs(friend.balance)}$
+          </p>
+        )}
+        {friend.balance > 0 && (
+          <p className="green">
+            You owe {friend.name} {Math.abs(friend.balance)}$
+          </p>
+        )}
+        {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+        <button className="button">Select</button>
+      </li>
     </div>
   );
 }
